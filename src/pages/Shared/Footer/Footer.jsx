b070dc99d2c50de062d49e87505a6fd3774163fc
Foysal Hossain <foysalhossain772@gmail.com>
2024-02-29 00:08:41 +0600

    banner img added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import logo from '../../../assets/logo.svg'
 
 function Footer() {
     return (
         <footer className="footer p-10 bg-base-200 text-base-content">
             <aside>
-                <img src={logo} alt="" />
+                <Link to="/">
+                    <img className="w-4/5 h-4/5" src={logo} alt="" />
+                </Link>
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </aside>
             <nav>
