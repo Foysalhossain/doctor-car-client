a6c799c6f0794b2e63ab337100f04c67403e47f9
Foysal Hossain <foysalhossain772@gmail.com>
2024-03-05 19:32:05 +0600

    delete a service booknig

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
 
-const BookingRow = ({ booking }) => {
-    const { img, date, service, price } = booking;
+
+const BookingRow = ({ booking, handleDelete }) => {
+    const { _id, img, date, service, price } = booking;
+
     return (
-        <div>
-            <tr>
-                <th>
-                    <label>
-                        <input type="checkbox" className="checkbox" />
-                    </label>
-                </th>
-                <td>
-                    <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
-                        </div>
+        <tr>
+            <th>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle btn-sm btn-outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+            <td>
+                <div className="avatar">
+                    <div className="mask mask-squircle w-16 h-16">
+                        {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
                     </div>
-                </td>
-                <td>{service}</td>
-                <td>{date}</td>
-                <td>{"$" + price}</td>
-                <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>
-        </div>
+                </div>
+            </td>
+            <td>{service}</td>
+            <td>{date}</td>
+            <td>{"$" + price}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
     );
 };
 
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 import BookingRow from './BookingRow';
+import Swal from 'sweetalert2';
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
@@ -11,7 +12,39 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, []);
+
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Are you sure?",
+                        text: "You won't be able to revert this!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Yes, delete it!"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    });
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    setBookings(remaining);
+                }
+            })
+    }
+
     return (
         <div>
             <h2>Your Booknig: {bookings.length}</h2>
@@ -25,9 +58,10 @@ const Bookings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
                             <th></th>
                         </tr>
                     </thead>
@@ -36,6 +70,7 @@ const Bookings = () => {
                             bookings.map(booking => <BookingRow
                                 key={booking._id}
                                 booking={booking}
+                                handleDelete={handleDelete}
                             ></BookingRow>)
                         }
                     </tbody>
